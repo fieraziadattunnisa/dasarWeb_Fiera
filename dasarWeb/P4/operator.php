f469b2060c4af7a1b0080f6aa789069d9ce8bfbe
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-09-17 10:04:32 +0700

    P4_praktikum3.step2

---
+++ dasarWeb/P4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+echo "Angka 1 : {$a}<br>";
+echo "Angka 2 : {$b}<br>";
+echo "Hasil tambah : {$hasilTambah}<br>";
+echo "Hasil kurang : {$hasilKurang}<br>";
+echo "Hasil kali : {$hasilKali}<br>";
+echo "Hasil Bagi : {$hasilBagi}<br>";
+echo "Sisa bagi : {$sisaBagi}<br>";
+echo "Hasil pangkat : {$pangkat}<br>";
+?>
