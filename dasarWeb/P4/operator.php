8e02d8ed7df03b01655572a7bbc1cb9c6829ebbb
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-09-17 10:18:38 +0700

    P4_Praktikum3.step5

--- dasarWeb/P4/operator.php
+++ dasarWeb/P4/operator.php
@@ -15,4 +15,17 @@ echo "Hasil kali : {$hasilKali}<br>";
 echo "Hasil Bagi : {$hasilBagi}<br>";
 echo "Sisa bagi : {$sisaBagi}<br>";
 echo "Hasil pangkat : {$pangkat}<br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+echo "<br>hasil sama : {$hasilSama}<br>";
+echo "hasil tidak sama : {$hasilTidakSama}<br>";
+echo "hasil lebih kecil : {$hasilLebihKecil}<br>";
+echo "hasil lebih besar : {$hasilLebihBesar}<br>";
+echo "hasil lebih kecil sama : {$hasilLebihKecilSama}<br>";
+echo "hasil lebih besar sama : {$hasilLebihBesarSama}<br>";
 ?>
