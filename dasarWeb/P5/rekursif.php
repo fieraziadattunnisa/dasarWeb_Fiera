e93db7061cf2a39e76b17b42655252f33c78c8c7
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-09-24 08:36:38 +0700

    P5_praktikum2.step 3,4

--- dasarWeb/P5/rekursif.php
+++ dasarWeb/P5/rekursif.php
@@ -1,7 +1,20 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Hallo Dunia!! <br>";
-    tampilkanHaloDunia();
+//function tampilkanHaloDunia() {
+   // echo "Hallo Dunia!! <br>";
+   // tampilkanHaloDunia();
+//}
+//tampilkanHaloDunia();
+
+//for ($i=1; $i <=25; $i++) {
+   // echo "perulangan ke-{$i} <br>";
+//}
+
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "perulangan ke -{$indeks}<br>";
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
-tampilkanHaloDunia();
+tampilkanAngka(20);
 ?>
