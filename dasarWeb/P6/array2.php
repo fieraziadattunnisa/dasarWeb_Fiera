8a006f5f6c550efe77fd0dd925a6a0e284442cc3
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-01 08:14:50 +0700

    P6_Praktikum2_step1 associative array perubahan

--- dasarWeb/P6/array2.php
+++ dasarWeb/P6/array2.php
@@ -1,31 +1,68 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Dosen</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f9f9f9;
+        }
+        table {
+            width: 50%;
+            margin: 50px auto;
+            border-collapse: collapse;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            background-color: #fff;
+        }
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+        th, td {
+            padding: 12px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+            font-weight: bold;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+    </style>
+</head>
 <body>
-    <?php
-   $Dosen = [
-    'nama' => 'Elok Nur Hamdana',
-    'domisili' => 'Malang',
-    'jenis_kelamin' => 'Perempuan'
-];
-?>
 
-<table>
-    <tr>
-        <th>Field</th>
-        <th>Data</th>
-    </tr>
-    <tr>
-        <td>Nama</td>
-        <td><?php echo $Dosen['nama']; ?></td>
-    </tr>
-    <tr>
-        <td>Domisili</td>
-        <td><?php echo $Dosen['domisili']; ?></td>
-    </tr>
-    <tr>
-        <td>Jenis Kelamin</td>
-        <td><?php echo $Dosen['jenis_kelamin']; ?></td>
-    </tr>
+    <?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+    ?>
+
+    <table>
+        <tr>
+            <th>Field</th>
+            <th>Data</th>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <td>Domisili</td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
     </table>
+
 </body>
 </html>
