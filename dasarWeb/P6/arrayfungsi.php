970a2b8958b5d9388e1e0137eb400a9ba47d8ce2
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-01 09:12:36 +0700

    P6_Fungsi bertingkat.step2, 3, 5

---
+++ dasarWeb/P6/arrayfungsi.php
@@ -0,0 +1,55 @@
+<?php
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+//function tampilkanMenuBertingkat(array $menu) {
+   //echo "<ul>";
+   //foreach ($menu as $key => $item) {
+       //echo "<li>{$item['nama']}</li>";
+    //}
+//}
+//tampilkanMenuBertingkat($menu);
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}</li>";
+        //Cek apakah item memiliki subMenu, jika ada, panggil fungsi ini lagi
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
+?>
