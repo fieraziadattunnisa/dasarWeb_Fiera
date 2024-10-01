f0743e2b2c91d270769b1ffba553b44150295096
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-01 08:38:34 +0700

    P6_Fungsi_step1

---
+++ dasarWeb/P6/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan() {
+   echo "Assalamualaikum, ";
+   echo "<br>Perkenalkan, nama saya Fiera<br>"; //Tulis sesuai nama kalian
+   echo "Senang berkenalan dengan anda <br>";
+}
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
