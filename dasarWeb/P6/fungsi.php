d508d8baa910eb694c3ec409d1a78bcb48a35e05
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-01 08:56:33 +0700

    P6_Fungsi_step 4, 7, 10, 13

--- dasarWeb/P6/fungsi.php
+++ dasarWeb/P6/fungsi.php
@@ -1,9 +1,40 @@
 <?php
-function perkenalan() {
-   echo "Assalamualaikum, ";
-   echo "<br>Perkenalkan, nama saya Fiera<br>"; //Tulis sesuai nama kalian
+// function perkenalan() {
+   // echo "Assalamualaikum, ";
+   // echo "<br>Perkenalkan, nama saya Fiera<br>"; //Tulis sesuai nama kalian
+   // echo "Senang berkenalan dengan anda <br>";
+//}
+// memanggil fungsi yang sudah dibuat
+//perkenalan();
+//perkenalan();
+
+//membuat fungsi
+//function perkenalan($nama, $salam) {
+   // echo $salam.", ";
+   // echo "<br>Perkenalkan, nama saya ".$nama." <br>";
+   // echo "Senang berkenalan dengan anda <br>";
+//}
+//memanggil fungsi yang sudah dibuat
+//perkenalan("Hamdana","hallo");
+//echo "<hr>";
+//$saya = "Fiera";
+//$ucapanSalam = "Selamat Pagi";
+
+//memanggil lagi
+//perkenalan($saya,$ucapanSalam);
+
+//membuat fungsi
+function perkenalan($nama, $salam = "Assalamualaikum") {
+   echo $salam.", ";
+   echo "<br>Perkenalakan, nama saya ".$nama."<br>";
    echo "Senang berkenalan dengan anda <br>";
 }
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana","Hallo");
+echo "<hr>";
+$saya = "Fiera";
+$ucapanSalam = "Selamat Pagi";
+
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
+?>
