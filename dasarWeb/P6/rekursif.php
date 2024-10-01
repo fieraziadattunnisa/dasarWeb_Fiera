3966ba06243c53b1b8b41e244f6084ffb6d83fba
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-01 09:06:55 +0700

    P6_Fungsi rekursif.step4

--- dasarWeb/P6/rekursif.php
+++ dasarWeb/P6/rekursif.php
@@ -5,16 +5,16 @@
 //}
 //tampilkanHaloDunia();
 
-for ($i=1; $i <=25; $i++) {
-  echo "perulangan ke-{$i} <br>";
-}
-
-//function tampilkanAngka (int $jumlah, int $indeks = 1) {
-    //echo "perulangan ke -{$indeks}<br>";
-    //panggil diri sendiri selama $indeks <= $jumlah
-    //if ($indeks < $jumlah) {
-        //tampilkanAngka($jumlah, $indeks + 1);
-    //}
+//for ($i=1; $i <=25; $i++) {
+  //echo "perulangan ke-{$i} <br>";
 //}
-//tampilkanAngka(20);
+
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "perulangan ke -{$indeks}<br>";
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
 ?>
