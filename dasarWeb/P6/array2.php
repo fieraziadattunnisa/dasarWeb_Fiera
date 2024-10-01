83d420b4b26e0e1fc50d2694f81941e7b759d755
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-01 07:58:29 +0700

    P6_Praktikum2.step1 associative array

---
+++ dasarWeb/P6/array2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+   $Dosen = [
+    'nama' => 'Elok Nur Hamdana',
+    'domisili' => 'Malang',
+    'jenis_kelamin' => 'Perempuan'
+];
+?>
+
+<table>
+    <tr>
+        <th>Field</th>
+        <th>Data</th>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td><?php echo $Dosen['nama']; ?></td>
+    </tr>
+    <tr>
+        <td>Domisili</td>
+        <td><?php echo $Dosen['domisili']; ?></td>
+    </tr>
+    <tr>
+        <td>Jenis Kelamin</td>
+        <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+    </tr>
+    </table>
+</body>
+</html>
