9f258fd0f85ce6dbc02371544c36a9e8a312b9a5
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-02 21:24:06 +0700

    tugas besar 2

--- dasarWeb/P6/tugas2.php
+++ dasarWeb/P6/tugas2.php
@@ -1,51 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-    <h1>Data Siswa</h1>
-    
-    <?php
-    // Data siswa
-    $siswa = [
-        ["nama" => "Andi", "umur" => 15, "kelas" => "10A", "alamat" => "malang"],
-        ["nama" => "Siti", "umur" => 16, "kelas" => "10B", "alamat" => "Batu"],
-        ["nama" => "Budi", "umur" => 15, "kelas" => "10A", "alamat" => "Dinoyo"],
-        ["nama" => "Anton", "umur" => 25, "kelas" => "15A", "alamat" => "Dinoyo"]
-    ];
-
-    // Menghitung rata-rata umur siswa
-    $total_umur = 0;
-    foreach ($siswa as $data) {
-        $total_umur += $data['umur'];
-    }
-    $rata_rata_umur = $total_umur / count($siswa);
-    ?>
-
-    <table>
-        <thead>
-            <tr>
-                <th>Nama</th>
-                <th>Umur</th>
-                <th>Kelas</th>
-                <th>Alamat</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($siswa as $data) : ?>
-            <tr>
-                <td><?= $data['nama']; ?></td>
-                <td><?= $data['umur']; ?></td>
-                <td><?= $data['kelas']; ?></td>
-                <td><?= $data['alamat']; ?></td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-
-    <h2>Rata-rata Umur Siswa: <?= number_format($rata_rata_umur, 2); ?> tahun</h2>
-
-</body>
+<html>
+    <head>
+        <style>
+           table {
+                width: 100%;
+                border-collapse: collapse;
+                margin: 20px 0;
+                font-size: 18px;
+              
+            }
+            th{
+                padding: 10px;
+                border: 1.5px solid #ddd;
+                text-align: left;
+            }
+            td, ul{
+                padding: 10px;
+                border: 1.5px solid #ddd;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Data Siswa</h1>
+        <table>
+        <tr>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Kelas</th>
+            <th>Alamat</th>
+        </tr>
+            <?php
+            $dataSiswa = array(
+                array("Andi", 15, "10A", "Malang"),
+                array("Siti", 16, "10B", "Batu"),
+                array("Budi", 15, "10A", "Dinoyo"),
+                array("Anton", 25, "15A", "Dinoyo")
+            );
+            echo "<tr>";
+            $totalUmur =0;
+            for ($i=0; $i < count($dataSiswa) ; $i++) { 
+                echo "<tr>";
+                echo "<td>" . $dataSiswa[$i][0] . "</td>";
+                echo "<td>" . $dataSiswa[$i][1] . "</td>";
+                echo "<td>" . $dataSiswa[$i][2] . "</td>";
+                echo "<td>" . $dataSiswa[$i][3] . "</td>";
+                echo "</tr>";
+             
+                $totalUmur += $dataSiswa[$i][1];
+            }
+            echo "</tr>";
+            $rataUmur = $totalUmur / count($dataSiswa) ;
+            ?>
+        </table>
+        <h2>Rata-rata Umur Siswa: <?php echo $rataUmur;?> tahun</h2>
+    </body>
 </html>
