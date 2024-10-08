510a390345ebb285f1c7881d22f275045664f188
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-08 08:30:25 +0700

    P7_praktikum2.step 2&4

---
+++ dasarWeb/P7/empty.php
@@ -0,0 +1,13 @@
+<?php
+//$myArray = array(); //Array kosong
+//if (empty($myArray)) {
+    //echo "array tidak terdefinisi atau kososng.";
+//} else {
+    //echo "array terdefinisi dan tidak kososng.";
+//}
+
+if (empty($nonExistrntVar)) {
+    echo "<br>variabel tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "variabel terdefinisi dan tidak kosong.<br>";
+}
