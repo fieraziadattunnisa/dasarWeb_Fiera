2c0f85135fbd31f2348310d1bfa084f2ce04f8a2
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-08 07:42:07 +0700

    P7_praktikum1.step5

--- dasarWeb/P7/isset.php
+++ dasarWeb/P7/isset.php
@@ -3,6 +3,13 @@ $umur;
 if(isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa.";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak diketahui.";
+    echo "Anda belum dewasa atau variabel 'umur' tidak diketahui.<br>";
+}
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama : " . $data["nama"];
+} else {
+    echo "variabel 'nama' tidak ditemukan dalam array.";
 }
 ?>
