ff8ce03959aa44ecf5a2172a0c82c9c72d2e33ed
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-15 08:59:40 +0700

    P8_praktikum 2.step 2, 3, 4 & 5

--- dasarWeb/P8/form_multiupload.php
+++ dasarWeb/P8/form_multiupload.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
+\<!DOCTYPE html>
 <html>
     <head>
-        <tittle>Multiupload Dokumen</tittle>
+        <title>Multiupload Gambar</title>
     </head>
     <body>
-        <h2>unggah dokumen</h2>
-        <form action="proses_upload" method="post" enctype="multipart/form-data">
-            <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx">
-            <input type="submit" value="unggah" />
+        <h2>Unggah Gambar</h2>
+        <form action="proses_upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="images[]" multiple="multiple" accept=".jpg, .jpeg, .png, .gif">
+            <input type="submit" value="Unggah" />
         </form>
     </body>
 </html>
--- dasarWeb/P8/proses_upload.php
+++ dasarWeb/P8/proses_upload.php
@@ -1,28 +1,35 @@
 <?php
 // Lokasi penyimpanan file yang diunggah
-$targetDirectory = "documents/";
+$targetDirectory = "images/";
 
 // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-if ($_FILES['files']['name'][0]) {
-    $totalFiles = count($_FILES['files']['name']);
+if ($_FILES['images']['name'][0]) {
+    $totalFiles = count($_FILES['images']['name']);
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
 
     // Loop melalui semua file yang diunggah
     for ($i = 0; $i < $totalFiles; $i++) {
-        $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $fileName;
+        $fileName = $_FILES['images']['name'][$i];
+        $fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = $targetDirectory . basename($fileName);
 
-        // Pindahkan file yang diunggah ke direktori penyimpanan
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        // Periksa apakah ekstensi file sesuai
+        if (in_array($fileType, $allowedExtensions)) {
+            // Pindahkan file yang diunggah ke direktori penyimpanan
+            if (move_uploaded_file($_FILES['images']['tmp_name'][$i], $targetFile)) {
+                echo "Gambar $fileName berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah gambar $fileName.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "Gambar $fileName memiliki ekstensi tidak valid.<br>";
         }
     }
 } else {
-    echo "Tidak ada file yang diunggah.";
+    echo "Tidak ada gambar yang diunggah.";
 }
 ?>
