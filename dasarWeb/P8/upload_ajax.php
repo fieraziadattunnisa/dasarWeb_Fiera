f916e8ee2a5fb8025093e551c83deeba7fb3960e
fieraziadattunnisa <fieraziadattunnisa@gmail.com>
2024-10-15 09:07:03 +0700

    P8_praktikum3.step4

---
+++ dasarWeb/P8/upload_ajax.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_FILES['file'])) {
+    $errors = array();
+    $fileName = $_FILES['file']['name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileType = $_FILES['file']['type'];
+    $fileExt = strtolower(end(explode(".", $_FILES['file']['name'])));
+    $extensions = array("pdf", "doc", "docx", "txt");
+
+    if (!in_array($fileExt, $extensions)) {
+        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+    }
+
+    if ($fileSize > 2097152) {
+        $errors[] = "Ukuran file tidak boleh lebih dari 2 MB";
+    }
+
+    if (empty($errors) == true) {
+        move_uploaded_file($_FILES['file']['tmp_name'], "documents/" . $fileName);
+        echo "File berhasil diunggah";
+    } else {
+        echo implode("", $errors);
+    }
+}
+?>
